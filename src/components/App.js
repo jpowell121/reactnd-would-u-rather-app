7a57525e711b72c77b01437ad711d67456813147
jpowell121 <jpowell@joelpowell.com>
2018-09-12 23:15:07 -0400

    chore: Further cleanup and created directory structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="center">
         App - starter
       </div>
     );
