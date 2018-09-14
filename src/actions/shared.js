3aa0cf261b628a4985847009e0de567249e311bd
jpowell121 <jpowell@joelpowell.com>
2018-09-13 23:36:36 -0400

    feat: Added initial actions for users, authedUser (currently hardcoded), and questions
     Added shared action handler as well

---
+++ src/actions/shared.js
@@ -0,0 +1,20 @@
+import { getInitialData } from "../utils/API";
+import { receiveUsers } from "./users";
+import { receiveQuestions } from "./questions";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_ID = 'tylermcginnis'; // todo this needs to be replaced when the real login is implemented
+
+export function handleInitialData() {
+  return(dispatch) => {
+    return(getInitialData()
+      .then((users, questions) => {
+        dispatch(receiveUsers(users));
+        dispatch(receiveQuestions(questions));
+        dispatch(setAuthedUser(AUTHED_ID));
+      }));
+  }
+}
+
+
+
