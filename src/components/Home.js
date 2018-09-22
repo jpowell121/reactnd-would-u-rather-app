fe415e7f8fcaf98872e287a4cc5a1bd16f195765
jpowell121 <jpowell@joelpowell.com>
2018-09-21 21:37:17 -0400

    feat: Partial implementation of Home page - show questions answered and unanswered
    Create tabbed area on home page - partial implementation - still needs more css work, but basic function is in place.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import QuestionFrame from './QuestionFrame'
 
+
 class Home extends Component {
 
+  handleTabClick = (event, tabName) => {
+
+    // hide all frames
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    // reset tabs
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // highlight the selected tab and mark it active.
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+
+  };
+
   render() {
+
     return (
-      <div><h1>Home</h1><QuestionFrame/></div>
+      <div>
+        <div className="tab">
+          <button className="tablinks active" id='answered' onClick={(e) => this.handleTabClick(e, 'answers')}>Answered Questions</button>
+          <button className="tablinks" id='unanswered' onClick={(e) => this.handleTabClick(e, 'questions')}>Unanswered Questions</button>
+        </div>
+
+        <div id="answers" className="tabcontent" style={{display: 'block'}}>
+          {this.props.answeredQuestions.map((question)=> <QuestionFrame id={question} />)}
+        </div>
+
+        <div id="questions" className="tabcontent">
+          {this.props.unansweredQuestions.map((question)=> <QuestionFrame id={question} />)}
+        </div>
+      </div>
     )
   }
 }
@@ -14,18 +49,20 @@ class Home extends Component {
 
 function mapStateToProps ({authedUser, users, questions}) {
 
-  console.log('----------------');
-  console.log('authedUser: ', authedUser);
-  console.log('users: ', users);
-  console.log('questions: ', questions);
-  console.log('----------------');
+  // this gets keys of the questions
+  const questionIds = Object.keys(questions);
+
+  // this gets keys of answers by authed users
+  const answeredQuestions = Object.keys(users[authedUser].answers);
+
+  // fine unanswered (remaining) questions
+  const unansweredQuestions = questionIds.filter(value => -1 === answeredQuestions.indexOf(value));
 
   return {
     authedUser: authedUser,
-    users: users,
-    questions: questions,
+    answeredQuestions: answeredQuestions,
+    unansweredQuestions: unansweredQuestions,
   }
-
 }
 
 export default connect(mapStateToProps)(Home)
--- src/components/QuestionFrame.js
+++ src/components/QuestionFrame.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 
 class QuestionFrame extends Component {
@@ -6,17 +7,17 @@ class QuestionFrame extends Component {
   render() {
     return (
       <div className="questionFrame">
-        <div className="qf-header">Joel Powell asks:</div>
+        <div className="qf-header">{this.props.author} asks:</div>
         <div className="qf-body">
           <img
-            src='https://tylermcginnis.com/would-you-rather/tyler.jpg'
-            alt={`Avatar of Joel`}
+            src={this.props.avatarURL}
+            alt={this.props.author}
             className='qf-avatar'
           />
           <div className="qf-divider"></div>
           <div className="qf-info">
             <span>Would you rather....</span>
-            <p>...be a front end developer...</p>
+            <p>...{this.props.questionText}...</p>
             <button className="qf-info-button">View Poll</button>
           </div>
         </div>
@@ -25,4 +26,22 @@ class QuestionFrame extends Component {
   }
 }
 
-export default QuestionFrame
+function mapStateToProps({authedUser, users, questions}, {id}) {
+
+
+
+  const authorId = questions[id].author;
+  const questionText = questions[id].optionOne.text;
+  const author = users[authorId].name;
+  const avatarURL = users[authorId].avatarURL;
+
+
+  return {
+    questionText: questionText,
+    author: author,
+    avatarURL: avatarURL,
+  }
+
+}
+
+export default connect(mapStateToProps)(QuestionFrame)
